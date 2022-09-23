f99c2acebb82ea79d6d3fc91dda8dd596bd1b9e7
Jack14188 <74159343+Jack14188@users.noreply.github.com>
2022-09-23 10:29:34 -0500

    Update OdometryCode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/OdometryCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/OdometryCode.java
@@ -2,4 +2,33 @@ package org.firstinspires.ftc.teamcode.DriveCode;
 
 public class OdometryCode {
     //test
+    
+    double ParaLeftEncoder = 0;
+    double ParaRightEncoder = 0;
+    double PerpEncoder = 0;
+    double LastParaLeftEncoder = 0;
+    double LastParaRightEncoder = 0;
+    double LastPerpEncoder = 0;
+    double ChangeParaLeftEncoder;
+    double ChangeParaRightEncodr;
+    double ChangePerpEncoder;
+    
+    public void OdoCalc(double RAWparallelLeftEncoder, double RAWparallelRightEncoder, double RAWPerpendicularEncoder){
+        
+        ParaLeftEncoder = RAWparallelLeftEncoder;
+        ParaRightEncoder = RAWparallelRightEncoder;
+        PerpEncoder = RAWPerpendicularEncoder;
+        
+        ChangeParaLeftEncoder = LastParaLeftEncoder - ParaLeftEncoder;
+        ChangeParaRightEncoder = LastParaRightEncoder - ParaRightEncoder;
+        ChangePerpEncoder = LastPerpEncoder - PerpEncoder;
+        
+        
+        
+        LastParaLeftEncoder = ParaleftEncoder;
+        LastParaRightEncoder = ParaRightEncoder;
+        LastPerpEncoder = PerpEncoder;
+        
+        
+    }
 }
